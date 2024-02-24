f8170ec032c53b934b777fd1386b2113f2b7276e
yuki designs <yukiosdesigns@gmail.com>
2024-02-24 16:18:03 +0300

    Added models, migrated, added urls, added settings

--- backend/converter/models.py
+++ backend/converter/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Currency(models.Model):
+    code =models.CharField(max_length=3, unique=True)
+    rate = models.FloatField()
+
+class UpdateLog(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
