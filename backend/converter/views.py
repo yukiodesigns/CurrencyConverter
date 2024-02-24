92bd9a962a158378043a316937707af2e2d1f410
yuki designs <yukiosdesigns@gmail.com>
2024-02-24 18:52:46 +0300

    Added comments

--- backend/converter/views.py
+++ backend/converter/views.py
@@ -13,6 +13,8 @@ from .serializer import CurrencySerializer
 logger = logging.getLogger(__name__)
 UPDATE_INTERVAL = 1 #hours
 EXCHANGE_RATE_API_URL = "https://api.exchangerate-api.com/v4/latest/USD"
+
+#View for conversion
 class ConvertCurrency(APIView):
     def get(self, request, from_currency, to_currency, amount):
         try:
@@ -23,14 +25,17 @@ class ConvertCurrency(APIView):
             return Response({"error":"Unexpected error"},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
     def update_rates_if_necessary(self):
+          # Get the timestamp of the last update
         try:
             last_updated = UpdateLog.objects.latest('updated_at')
         except UpdateLog.DoesNotExist:
             last_updated = None
+        # Check if rates need to be updated based on the update interval
         if not last_updated or last_updated.updated_at < timezone.now() - timedelta(hours=UPDATE_INTERVAL):
             self.update_rates()
 
     def update_rates(self):
+        # Fetch latest exchange rates from API and update database
         response = requests.get(EXCHANGE_RATE_API_URL)
         data = response.json()
         if 'rates' not in data:
@@ -41,17 +46,19 @@ class ConvertCurrency(APIView):
         UpdateLog.objects.update_or_create(id=1, defaults={'updated_at':timezone.now()})
     
     def perform_conversion(self, from_currency, to_currency, amount):
+        #Getting the country currency and their rates
         try:
             from_rate = Currency.objects.get(code=from_currency.upper()).rate
             to_rate = Currency.objects.get(code=to_currency.upper()).rate
         except Currency.DoesNotExist:
             return Response({"error":"Currency not found"},status=status.HTTP_400_BAD_REQUEST)
         
+        #Performing the conversion
         converted_amount = (float(amount) / from_rate) * to_rate
         return Response({'converted_amount':converted_amount})
 
 
-
+#View for displaying currencies
 class CurrencyList(generics.ListAPIView):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
