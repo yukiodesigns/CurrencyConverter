9fc78cd209e14e6ddb4808668ac6f2573cd42593
yuki designs <yukiosdesigns@gmail.com>
2024-02-24 16:26:03 +0300

    Created serializer and added view

--- backend/converter/views.py
+++ backend/converter/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Currency
+from .serializer import CurrencySerializer
 # Create your views here.
+
+class CurrencyList(generics.ListAPIView):
+    queryset = Currency.objects.all()
+    serializer_class = CurrencySerializer
